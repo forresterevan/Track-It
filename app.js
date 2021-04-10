2b55d7170442bac4c63251c0109908bbcabf3b39
Evan Forrester <forresterevan@hotmail.com>
2021-04-10 16:05:54 +0800

    v1

---
+++ app.js
@@ -0,0 +1,15 @@
+let config = require('./key')
+App({
+  onLaunch: function() {
+    wx.BaaS = requirePlugin('sdkPlugin')
+    wx.BaaS.wxExtend(wx.login, wx.getUserInfo, wx.requestPayment)
+    let clientID = config.appKey
+    wx.BaaS.init(clientID)
+
+    wx.BaaS.auth.loginWithWechat().then(user => {
+      console.log(user)
+    }, err => {
+      console.log(err)
+  })
+  }
+})
