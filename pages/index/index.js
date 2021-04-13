571a5bc4a2af9dd1e86df1f326e594053ab27006
Evan Forrester <forresterevan@hotmail.com>
2021-04-13 20:58:07 +0800

    Landing and Index Page v1

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,10 +2,19 @@ const app = getApp()
 
 Page({
   data: {
-    
   },
-  
   onLoad() {
-    
+    this.getJobs()
   },
+  getJobs: function() {
+    let Jobs = new wx.BaaS.TableObject('jobs'); 
+    Jobs.find().then((res) => {
+      this.setData({ jobs: res.data.objects })
+    })
+  },
+  navigateToShow: function(e) {
+    wx.navigateTo({
+      url: `/pages/show/show?id=${e.currentTarget.dataset.id}`,
+    })
+  }
 })
