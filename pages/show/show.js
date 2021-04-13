ac7db346cda102c2298cfbc3518bf3a9d79f6a42
Gabriel <2002.gabmachado@gmail.com>
2021-04-13 22:01:11 +0800

    Fixed everything only missing CSS

--- pages/show/show.js
+++ pages/show/show.js
@@ -1,7 +1,8 @@
 // pages/show/show.js
 Page({
   data: {
-
+    currentUser: null,
+    captured: false
   },
   getJobs: function (id) {
     let Jobs = new wx.BaaS.TableObject('jobs')
@@ -17,6 +18,55 @@ Page({
  
   onLoad: function (options) {
     let id = options.id
+    this.checklogin();
+    this.setData({job_id: id})
     this.getJobs(id);
+    this.checkLikedJob();
+
   },
+  saveToDatabase: function() {
+    let favoriteJobs = new wx.BaaS.TableObject("favorite_jobs")
+    let fJ = favoriteJobs.create()
+    fJ.set({user_id: this.data.currentUser.id, job_id: this.data.job.id}).save().then(res => {
+      console.log(res)
+      this.setData({fav: true})
+    })
+},
+ //log-in
+ onGotUserInfo: function(data) {
+  console.log(data)
+  wx.BaaS.auth.loginWithWechat(data).then(user => {
+      wx.setStorageSync('user', user)
+      this.setData({
+        currentUser: user
+      })
+      this.checkLikedJob()
+    }, err => {
+      console.log(err)
+  })
+},
+checklogin: function () {
+  let user = wx.getStorageSync('user')
+  if (user) {
+    this.setData({
+      currentUser: user
+    })
+    this.checkLikedJob()
+  }
+  console.log(user)
+},
+//check Job
+checkLikedJob: function() {
+  let user_id = this.data.currentUser.id
+  let job_id = this.data.job_id
+  let favoriteJob = new wx.BaaS.TableObject("favorite_jobs")
+  let query = new wx.BaaS.Query()
+  query.compare('user_id', '=', user_id)
+  query.compare('job_id', '=', job_id)
+  favoriteJob.setQuery(query).find().then(res => {
+    console.log(res)
+    let fav = res.data.objects.length > 0 
+    this.setData({favoriteJob: res.data.objects, fav: fav})
+  })
+}
 })
