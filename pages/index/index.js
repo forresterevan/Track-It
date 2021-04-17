91ae9a29d929ac677e61f8e6e6fd55e89de6dc37
Evan Forrester <forresterevan@hotmail.com>
2021-04-17 13:34:34 +0800

    Morning Code 4/17

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,7 @@
 const app = getApp()
 Page({
   data: {
+    jobType: false // non-teaching
   },
   onLoad() {
     this.getJobs()
@@ -9,17 +10,28 @@ Page({
   getJobs: function() {
     let Jobs = new wx.BaaS.TableObject('jobs'); 
     Jobs.find().then((res) => {
-      this.getCities(res.data.objects) 
-      this.setData({ jobs: res.data.objects })
+      this.getUniqFields(res.data.objects) 
+      this.setData({ jobs: res.data.objects, originalJobs: res.data.objects })
     })
   },
-  getCities: function(jobs){
+  getUniqFields: function(jobs){
     let cities = jobs.map( job => job.city)
-
+    let ranges = jobs.map( job => job.salary_range)
     let cityObj = {}
+    let rangeObj = {}
     cities.forEach(city => cityObj[city] = city)
+    ranges.forEach(range => rangeObj[range] = range)
     let uniqCities = Object.keys(cityObj)
-    this.setData({jobsLocation: uniqCities})
+    let uniqRanges = Object.keys(rangeObj)
+    this.setData({jobsLocation: uniqCities, salaryRanges: uniqRanges})
+  },
+  jobTypeChange: function (e) {
+    console.log(e)
+    let chosenJobType = e.detail.value
+    let fliteredJobs = this.data.originalJobs.filter(job => job.job_type == chosenJobType)
+    this.setData({chosenJobType: chosenJobType, jobs: fliteredJobs}) 
+  },
+  filterJobsLocally: function() {
   },
   navigateToShow: function(e) {
     wx.navigateTo({
@@ -32,7 +44,6 @@ Page({
       url: '/pages/form/form',
     })
   },
-
   userInfoHandler(data) {
     wx.BaaS.auth.loginWithWechat(data).then(user => {
         wx.setStorageSync('user', user)
@@ -42,5 +53,16 @@ Page({
       }, err => {
         console.log(err)
     })
-  }
+  },
+  bindCityChange: function (e) {
+    console.log(e)
+    let cityIndex = e.detail.value
+    let fliteredJobs = this.data.originalJobs.filter(job => job.city == this.data.jobsLocation[cityIndex])
+    this.setData({cityIndex: cityIndex, jobs: fliteredJobs})
+  },
+  bindSalaryChange: function (e) {
+    console.log(e)
+    let salaryIndex = e.detail.value
+    this.setData({salaryIndex: salaryIndex})
+  },
 })
